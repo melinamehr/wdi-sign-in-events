9e275da94776d5683b0fe079d3b1c7e5a84cfc04
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-07-11 16:32:52 -0400

    modal displays and also blocks

---
+++ js/main.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+
+  var signIn = document.querySelector('.signin')
+  var modal = document.querySelector('.modal')
+  var close = document.querySelector('.close')
+  var submit = document.querySelector('.submit')
+  var input = document.querySelectorAll('input')
+
+  signIn.addEventListener('click', function() {
+    modal.style.display = 'inline';
+  });
+
+  close.addEventListener('click', function() {
+    modal.style.display = 'none';
+  })
+
+  submit.addEventListener('click', function() {
+    submit.style.display =
+  })
+});
